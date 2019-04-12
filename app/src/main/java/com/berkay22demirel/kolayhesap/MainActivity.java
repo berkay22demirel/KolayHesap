4f30c28d43baeb81f926cbc31e94c30a3baee337
berkay <berkay_demirel22@hotmail.com>
2019-04-12 23:58:52 +0300

    add animation

--- app/src/main/java/com/berkay22demirel/kolayhesap/MainActivity.java
+++ app/src/main/java/com/berkay22demirel/kolayhesap/MainActivity.java
@@ -1,7 +1,11 @@
 package com.berkay22demirel.kolayhesap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        showAnimation();
+    }
+
+    private void showAnimation() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.animation_start);
+        ImageView girisLogo = (ImageView) findViewById(R.id.imageViewAnimationLogo);
+        anim.reset();
+        girisLogo.clearAnimation();
+        girisLogo.startAnimation(anim);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
