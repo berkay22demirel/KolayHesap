b9df6cd5aaf4cbf709d7086df7f55807faa3893f
berkay <berkay_demirel22@hotmail.com>
2019-04-20 13:51:53 +0300

    add home activity design and toolbar design

--- app/src/main/java/com/berkay22demirel/kolayhesap/HomeActivity.java
+++ app/src/main/java/com/berkay22demirel/kolayhesap/HomeActivity.java
@@ -1,13 +1,56 @@
 package com.berkay22demirel.kolayhesap;
 
+import android.app.ActionBar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        MenuItem searchViewItem = menu.findItem(R.id.toolbar_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchViewItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.clearFocus();
+                Toast.makeText(HomeActivity.this, " Ne Oldu", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Toast.makeText(HomeActivity.this, newText + " Arandı", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.toolbar_settings:
+                Toast.makeText(this, "Ayarlar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.toolbar_about_me:
+                Toast.makeText(this, "Hakkımda", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
